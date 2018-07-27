de5ea0106a8bc9755f764bf0f3ef4860581a33df
chunkydonut21 <shivammahe21@gmail.com>
2018-07-27 19:32:47 +0530

    layout completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import Header from './Header';
+import CalculatorDisplay from './CalculatorDisplay';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <div>
-        React App
+      <div id="wrap">
+        <Header />
+        <div id="inner">
+          <CalculatorDisplay />
+          <button className="button button-dark">AC</button>
+          <button className="button button-dark">±</button>
+          <button className="button button-dark">%</button>
+          <button className="button button-orange">/</button>
+          <button className="button button-light">7</button>
+          <button className="button button-light">8</button>
+          <button className="button button-light">9</button>
+          <button className="button button-orange">x</button>
+          <button className="button button-light">4</button>
+          <button className="button button-light">5</button>
+          <button className="button button-light">6</button>
+          <button className="button button-orange">-</button>
+          <button className="button button-light">1</button>
+          <button className="button button-light">2</button>
+          <button className="button button-light">3</button>
+          <button className="button button-orange">+</button>
+          <button className="button button-light" style={{ flex: 2 }}>0</button>
+          <button className="button button-light">.</button>
+          <button className="button button-orange">=</button>
+        </div>
       </div>
     );
   }
---
+++ src/components/CalculatorDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CalculatorDisplay extends React.Component {
+  render() {
+    return (
+      <div className="display-wrap">
+        <h1>Calculator</h1>
+      </div>
+    );
+  }
+}
