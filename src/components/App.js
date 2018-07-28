f849a69069160c9c896dc0279d7b84d301ceb8e7
chunkydonut21 <shivammahe21@gmail.com>
2018-07-28 22:43:34 +0530

    added more functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,20 @@ import CalculatorDisplay from './CalculatorDisplay';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { displayValue: '0' };
+    this.state = { displayValue: '0', value: 0, operatorPressed: false, operator: null };
   }
 
   handleInput = (input) => {
-    this.setState({ 
-      displayValue: this.state.displayValue === '0' ? String(input) : this.state.displayValue + String(input)
-    });
+    if(this.state.operatorPressed) {
+      this.setState({
+        displayValue: String(input),
+        operatorPressed: false
+      });
+    } else {
+      this.setState({
+        displayValue: this.state.displayValue === '0' ? String(input) : this.state.displayValue + String(input)
+      });
+    }
   }
 
   handleClear = () => {
@@ -20,7 +27,9 @@ class App extends React.Component {
 
   handleDot = () => {
     const { displayValue } = this.state;
-    if(displayValue.indexOf('.') === -1) {
+    if(this.state.operatorPressed) {
+      this.setState({ displayValue: '.', operatorPressed: false });
+    } else if(displayValue.indexOf('.') === -1) {
       this.setState({ displayValue: this.state.displayValue + '.' }); 
     }
   }
@@ -34,32 +43,62 @@ class App extends React.Component {
     const { displayValue } = this.state;
     this.setState({ displayValue: String(parseFloat(displayValue)/100) });
   }
- 
+
+  handleOperation = (nextOperator) => {    
+    const { value, displayValue, operator } = this.state;
+
+    const operations = {
+      '/': (prevValue, nextValue) => prevValue / nextValue,
+      '*': (prevValue, nextValue) => prevValue * nextValue,
+      '+': (prevValue, nextValue) => prevValue + nextValue,
+      '-': (prevValue, nextValue) => prevValue - nextValue,
+      '=': (prevValue, nextValue) => nextValue
+    }
+
+    if (operator == null) {
+      this.setState({
+        value: parseFloat(displayValue)
+      })
+    } else if (operator) {
+      const newValue = operations[operator](value || 0, parseFloat(displayValue));
+      
+      this.setState({
+        value: newValue,
+        displayValue: String(newValue)
+      });
+    }
+    
+    this.setState({
+      operatorPressed: true,
+      operator: nextOperator
+    });
+  }
+  
   render() {
     return (
       <div id="wrap">
         <Header />
         <div id="inner">
           <CalculatorDisplay value={this.state.displayValue} />
-          <button className="button button-dark" onClick={() => this.handleClear()}>AC</button>
+          <button className="button button-dark" id="clear" onClick={() => this.handleClear()}>AC</button>
           <button className="button button-dark" onClick={() => this.handleToggle()}>±</button>
           <button className="button button-dark" onClick={() => this.handlePercent()}>%</button>
-          <button className="button button-orange">/</button>
-          <button className="button button-light" onClick={() => this.handleInput(7)}>7</button>
-          <button className="button button-light" onClick={() => this.handleInput(8)}>8</button>
-          <button className="button button-light" onClick={() => this.handleInput(9)}>9</button>
-          <button className="button button-orange">x</button>
-          <button className="button button-light" onClick={() => this.handleInput(4)}>4</button>
-          <button className="button button-light" onClick={() => this.handleInput(5)}>5</button>
-          <button className="button button-light" onClick={() => this.handleInput(6)}>6</button>
-          <button className="button button-orange">-</button>
-          <button className="button button-light" onClick={() => this.handleInput(1)}>1</button>
-          <button className="button button-light" onClick={() => this.handleInput(2)}>2</button>
-          <button className="button button-light" onClick={() => this.handleInput(3)}>3</button>
-          <button className="button button-orange">+</button>
-          <button className="button button-light" style={{ flex: 2 }} onClick={() => this.handleInput(0)}>0</button>
-          <button className="button button-light" onClick={() => this.handleDot()}>.</button>
-          <button className="button button-orange">=</button>
+          <button className="button button-orange" id="divide" onClick={() => this.handleOperation('/')}>/</button>
+          <button className="button button-light" id="seven" onClick={() => this.handleInput(7)}>7</button>
+          <button className="button button-light" id="eight" onClick={() => this.handleInput(8)}>8</button>
+          <button className="button button-light" id="nine" onClick={() => this.handleInput(9)}>9</button>
+          <button className="button button-orange" id="multiply" onClick={() => this.handleOperation('*')}>x</button>
+          <button className="button button-light" id="four" onClick={() => this.handleInput(4)}>4</button>
+          <button className="button button-light" id="five" onClick={() => this.handleInput(5)}>5</button>
+          <button className="button button-light" id="six" onClick={() => this.handleInput(6)}>6</button>
+          <button className="button button-orange" id="subtract" onClick={() => this.handleOperation('-')}>-</button>
+          <button className="button button-light" id="one" onClick={() => this.handleInput(1)}>1</button>
+          <button className="button button-light" id="two" onClick={() => this.handleInput(2)}>2</button>
+          <button className="button button-light" id="three" onClick={() => this.handleInput(3)}>3</button>
+          <button className="button button-orange" id="add"  onClick={() => this.handleOperation('+')}>+</button>
+          <button className="button button-light" style={{ flex: 2 }} id="zero" onClick={() => this.handleInput(0)}>0</button>
+          <button className="button button-light" id="decimal" onClick={() => this.handleDot()}>.</button>
+          <button className="button button-orange" id="equals" onClick={() => this.handleOperation('=')}>=</button>
         </div>
       </div>
     );
--- src/components/CalculatorDisplay.js
+++ src/components/CalculatorDisplay.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class CalculatorDisplay extends React.Component {
   render() {
     return (
-      <div className="display-wrap">
-        <h1>{this.props.value}</h1>
+      <div id="display" className="display-wrap">
+        {this.props.value}
       </div>
     );
   }
