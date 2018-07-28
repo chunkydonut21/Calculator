c20da194e8d4d9c8545084ccd33cbe64387c84a7
chunkydonut21 <shivammahe21@gmail.com>
2018-07-28 10:42:52 +0530

    added some functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,31 +5,60 @@ import CalculatorDisplay from './CalculatorDisplay';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { displayValue: '0' };
   }
+
+  handleInput = (input) => {
+    this.setState({ 
+      displayValue: this.state.displayValue === '0' ? String(input) : this.state.displayValue + String(input)
+    });
+  }
+
+  handleClear = () => {
+    this.setState({ displayValue: '0' });
+  }
+
+  handleDot = () => {
+    const { displayValue } = this.state;
+    if(displayValue.indexOf('.') === -1) {
+      this.setState({ displayValue: this.state.displayValue + '.' }); 
+    }
+  }
+
+  handleToggle = () => {
+    const { displayValue } = this.state;
+    displayValue.charAt(0) === '-' ? this.setState({ displayValue: displayValue.substr(1)}) : this.setState({ displayValue: '-' + displayValue});
+  }
+
+  handlePercent = () => {
+    const { displayValue } = this.state;
+    this.setState({ displayValue: String(parseFloat(displayValue)/100) });
+  }
+ 
   render() {
     return (
       <div id="wrap">
         <Header />
         <div id="inner">
-          <CalculatorDisplay />
-          <button className="button button-dark">AC</button>
-          <button className="button button-dark">±</button>
-          <button className="button button-dark">%</button>
+          <CalculatorDisplay value={this.state.displayValue} />
+          <button className="button button-dark" onClick={() => this.handleClear()}>AC</button>
+          <button className="button button-dark" onClick={() => this.handleToggle()}>±</button>
+          <button className="button button-dark" onClick={() => this.handlePercent()}>%</button>
           <button className="button button-orange">/</button>
-          <button className="button button-light">7</button>
-          <button className="button button-light">8</button>
-          <button className="button button-light">9</button>
+          <button className="button button-light" onClick={() => this.handleInput(7)}>7</button>
+          <button className="button button-light" onClick={() => this.handleInput(8)}>8</button>
+          <button className="button button-light" onClick={() => this.handleInput(9)}>9</button>
           <button className="button button-orange">x</button>
-          <button className="button button-light">4</button>
-          <button className="button button-light">5</button>
-          <button className="button button-light">6</button>
+          <button className="button button-light" onClick={() => this.handleInput(4)}>4</button>
+          <button className="button button-light" onClick={() => this.handleInput(5)}>5</button>
+          <button className="button button-light" onClick={() => this.handleInput(6)}>6</button>
           <button className="button button-orange">-</button>
-          <button className="button button-light">1</button>
-          <button className="button button-light">2</button>
-          <button className="button button-light">3</button>
+          <button className="button button-light" onClick={() => this.handleInput(1)}>1</button>
+          <button className="button button-light" onClick={() => this.handleInput(2)}>2</button>
+          <button className="button button-light" onClick={() => this.handleInput(3)}>3</button>
           <button className="button button-orange">+</button>
-          <button className="button button-light" style={{ flex: 2 }}>0</button>
-          <button className="button button-light">.</button>
+          <button className="button button-light" style={{ flex: 2 }} onClick={() => this.handleInput(0)}>0</button>
+          <button className="button button-light" onClick={() => this.handleDot()}>.</button>
           <button className="button button-orange">=</button>
         </div>
       </div>
--- src/components/CalculatorDisplay.js
+++ src/components/CalculatorDisplay.js
@@ -4,7 +4,7 @@ export default class CalculatorDisplay extends React.Component {
   render() {
     return (
       <div className="display-wrap">
-        <h1>Calculator</h1>
+        <h1>{this.props.value}</h1>
       </div>
     );
   }
